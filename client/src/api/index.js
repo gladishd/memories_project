0d4e01269b0404d51dd6ec345ba4fbc2f6da993d
gladishd <gladish.dean@gmail.com>
2023-01-08 00:25:24 -0600

    git adds all

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,19 @@ import axios from 'axios';
 
 const url = 'http://localhost:5000/posts';
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost); 
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const fetchPosts = () => {
+  console.log("We are fetching with the 'input'", url)
+  return axios.get(url)
+};
+export const createPost = (newPost) => {
+  console.log("The url is what is it? provided in the global context. The real input we have is ", newPost)
+  axios.post(url, newPost);
+}
+export const updatePost = (id, updatedPost) => {
+  console.log("We can still update the post but we do it with id", id, "and updatedPost ", updatedPost)
+  axios.patch(`${url}/${id}`, updatedPost);
+}
+export const deletePost = (id) => {
+  console.log("We are about to delete a post at this Id", id)
+  axios.delete(`${url}/${id}`);
+}
